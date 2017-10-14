8226e656d6d49c4f50895176c6cd9e9af3278e7d
Henrik Engström <me@henrikengstrom.com>
2017-10-14 22:55:29 +0200

    Initial Commit

---
+++ Firlib/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Firlib
+//
+//  Created by Henrik Engström on 2017-10-14.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
